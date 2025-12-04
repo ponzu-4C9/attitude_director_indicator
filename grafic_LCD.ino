ce3bb67d79f60ec6ec7dc96496c8c2b35a9f2797
ponzu-4C9 <baijingseiya@gmail.com>
2025-12-04 19:28:47 +0900

    first commit

---
+++ grafic_LCD.ino
@@ -0,0 +1,12 @@
+#include <TFT_eSPI.h>
+TFT_eSPI tft = TFT_eSPI(); 
+void setup() {
+  Serial.begin(115200);
+  tft.init();
+  tft.setRotation(1); 
+  tft.fillScreen(TFT_BLACK); 
+  tft.setTextColor(TFT_WHITE, TFT_BLACK); 
+  tft.drawString("Hello ILI9488 World!", 50, 100, 4); 
+}
+void loop() {
+}
