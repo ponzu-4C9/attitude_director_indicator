a340647ca6ff01c51aa72a5638bbdbb6763dc9e0
ponzu-4C9 <baijingseiya@gmail.com>
2025-12-10 22:58:53 +0900

    方眼のデータ構造の定義とアフィン変換の実装

--- grafic_LCD.ino
+++ grafic_LCD.ino
@@ -8,61 +8,6 @@
 TFT_eSPI tft = TFT_eSPI();
 ICM20948_WE myIMU = ICM20948_WE(ICM20948_ADDR);
 
-void setup() {
-  Wire.begin();
-  Serial.begin(115200);
-  if (!myIMU.init()) {
-    Serial.println("ICM20948 does not respond");
-  } else {
-    Serial.println("ICM20948 is connected");
-  }
-  myIMU.autoOffsets();
-  myIMU.setAccRange(ICM20948_ACC_RANGE_2G);
-  myIMU.setAccDLPF(ICM20948_DLPF_6);
-  myIMU.setAccSampleRateDivider(10);
-
-  tft.begin();
-  tft.setRotation(0);
-  tft.fillScreen(TFT_BLACK);
-}
-
-typedef struct {
-  float x;
-  float y;
-  float z;
-  float w;
-} Vector;
-
-#define SIZE 4
-void multiply_matrix(const float A[SIZE][SIZE], const float B[SIZE][SIZE], float C[SIZE][SIZE]) {
-    // 行列の積 C[i][j] = Sum(A[i][k] * B[k][j])
-    for (int i = 0; i < SIZE; i++) { // 結果Cの行 (i)
-        for (int j = 0; j < SIZE; j++) { // 結果Cの列 (j)
-            C[i][j] = 0.0f; // 初期化
-            for (int k = 0; k < SIZE; k++) { // 共通のインデックス (k)
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-}
-Vector transform_vector(const float M[SIZE][SIZE], const Vector V) {
-    Vector result;
-    
-    // X' = M[0][0]*x + M[0][1]*y + M[0][2]*z + M[0][3]*w
-    result.x = M[0][0] * V.x + M[0][1] * V.y + M[0][2] * V.z + M[0][3] * V.w;
-
-    // Y' = M[1][0]*x + M[1][1]*y + M[1][2]*z + M[1][3]*w
-    result.y = M[1][0] * V.x + M[1][1] * V.y + M[1][2] * V.z + M[1][3] * V.w;
-
-    // Z' = M[2][0]*x + M[2][1]*y + M[2][2]*z + M[2][3]*w
-    result.z = M[2][0] * V.x + M[2][1] * V.y + M[2][2] * V.z + M[2][3] * V.w;
-
-    // W' = M[3][0]*x + M[3][1]*y + M[3][2]*z + M[3][3]*w
-    result.w = M[3][0] * V.x + M[3][1] * V.y + M[3][2] * V.z + M[3][3] * V.w;
-
-    return result;
-}
-
 void applyMatrix(double a, double b, double c, double d,
                  double *x, double *y) {
 
@@ -156,8 +101,6 @@ void DrawFixedGUI(int lineSpacing = 5) {
   DrawTriangle(-ex, ey, -fx, fy, -fx, -fy, baseTriangleColor);
 
 
-
-
   //ロール目盛りの生成
   uint16_t rollScaleColor = TFT_WHITE;
   const double gy_dis = 200;  //原点からの円弧の距離
@@ -187,6 +130,115 @@ void DrawFixedGUI(int lineSpacing = 5) {
 }
 
 
+typedef struct {
+  float x;
+  float y;
+  float z;
+  float w;
+} Vector;
+
+typedef struct {
+  Vector a;
+  Vector b;
+} Line;
+
+#define SIZE 4
+void multiply_matrix(const float A[SIZE][SIZE], const float B[SIZE][SIZE], float C[SIZE][SIZE]) {
+  // 行列の積 C[i][j] = Sum(A[i][k] * B[k][j])
+  for (int i = 0; i < SIZE; i++) {      // 結果Cの行 (i)
+    for (int j = 0; j < SIZE; j++) {    // 結果Cの列 (j)
+      C[i][j] = 0.0f;                   // 初期化
+      for (int k = 0; k < SIZE; k++) {  // 共通のインデックス (k)
+        C[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+}
+
+void applyMatrix4(const float M[SIZE][SIZE], Vector *V) {
+  Vector temp;
+  temp.x = M[0][0] * V->x + M[0][1] * V->y + M[0][2] * V->z + M[0][3] * V->w;
+  temp.y = M[1][0] * V->x + M[1][1] * V->y + M[1][2] * V->z + M[1][3] * V->w;
+  temp.z = M[2][0] * V->x + M[2][1] * V->y + M[2][2] * V->z + M[2][3] * V->w;
+  temp.w = M[3][0] * V->x + M[3][1] * V->y + M[3][2] * V->z + M[3][3] * V->w;
+  V->x = temp.x;
+  V->y = temp.y;
+  V->z = temp.z;
+  V->w = temp.w;
+}
+
+const double z_max = 1000;
+const int h_num = 10;
+const double rs = z_max / h_num;  //横線間隔
+const int v_num = 10;
+
+Line hline[h_num];
+void hlineinitializer() {  //これは直線だからとりあえず長さを1の線分として定義
+  for (int i = 0; i < h_num; i++) {
+    hline[i].a.x = 0;
+    hline[i].b.x = 1;
+
+    hline[i].a.y = hline[i].b.y = 0;
+
+    hline[i].a.z = hline[i].b.z = i * rs;
+
+    hline[i].a.w = hline[i].b.w = 1;
+  }
+}
+Line vline[v_num];  //始点z_max,終点0の線分
+void vlineinitializer() {
+  for (int i = 0; i < v_num; i++) {
+    vline[i].a.x = vline[i].b.x = i * rs;
+    vline[i].a.y = vline[i].b.y = 0;
+    vline[i].a.z = z_max;
+    vline[i].b.z = 0;
+    vline[i].a.w = vline[i].b.w = 1;
+  }
+}
+
+
+void setup() {
+  Wire.begin();
+  Serial.begin(115200);
+  if (!myIMU.init()) {
+    Serial.println("ICM20948 does not respond");
+  } else {
+    Serial.println("ICM20948 is connected");
+  }
+  myIMU.autoOffsets();
+  myIMU.setAccRange(ICM20948_ACC_RANGE_2G);
+  myIMU.setAccDLPF(ICM20948_DLPF_6);
+  myIMU.setAccSampleRateDivider(10);
+
+  tft.begin();
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
+
+  //ここで方眼を初期化
+  hlineinitializer();
+  vlineinitializer();
+}
+
+float T[SIZE][SIZE] = {
+  { 1, 0, 0, 0 },
+  { 0, 1, 0, 0 },
+  { 0, 0, 1, 0 },
+  { 0, 0, 0, 1 }
+} 
+float Rx[SIZE][SIZE] = {
+  { 1, 0, 0, 0 },
+  { 0, 1, 0, 0 },
+  { 0, 0, 1, 0 },
+  { 0, 0, 0, 1 }
+} 
+float Rz[SIZE][SIZE] = {
+  { 1, 0, 0, 0 },
+  { 0, 1, 0, 0 },
+  { 0, 0, 1, 0 },
+  { 0, 0, 0, 1 }
+} 
+float M_temp[SIZE][SIZE];
+float M[SIZE][SIZE];
 
 int y0a = 0;
 int y1a = 0;
@@ -195,7 +247,7 @@ void loop() {
   myIMU.readSensor();
 
   float pitch = myIMU.getPitch();
-  float roll = myIMU.getRoll() * PI / 180.0;
+  float roll = myIMU.getRoll();
   Serial.print("Pitch:  ");
   Serial.print(pitch, 10);
   Serial.print("  |  Roll: ");
@@ -204,9 +256,31 @@ void loop() {
 
   float Spacing = 10;
 
+  float h = 10;  //高度単位は画面px
+  T[1][3] = -h;  //下にh文下げる
+
+  float fai = -pitch * PI / 180;
+  Rx[1][1] = cos(fai);Rx[1][2] = sin(fai);
+  Rx[2][1] = -sin(fai);Rx[2][2] = cos(fai)
+
+  float theta = -roll * PI / 180.0;
+  Rz[0][0] = cos(theta);Rz[1][0] = sin(theta);
+  Rz[0][1] = -sin(theta);Rz[1][1] = cos(theta);
+
+  multiply_matrix(Rz,Rx,M_temp);
+  multiply_matrix(M_temp,T,M);
+
+  for (int i = 0; i < h_num; i++) {
+    applyMatrix4(M,hline[i].a);
+    applyMatrix4(M,hline[i].b);
+  }
+  for (int i = 0; i < v_num; i++) {
+    applyMatrix4(M,vline[i].a);
+    applyMatrix4(M,vline[i].b);
+  }
   DrawLine(-160, y0a, 160, y1a, TFT_BLACK);
-  y0a = -tan(roll) * (-160) - Spacing * pitch;
-  y1a = -tan(roll) * (160) - Spacing * pitch;
+  y0a = tan(theta) * (-160) - Spacing * pitch;
+  y1a = tan(theta) * (160) - Spacing * pitch;
   DrawLine(-160, y0a, 160, y1a, TFT_GREEN);
 
   DrawFixedGUI(Spacing);
